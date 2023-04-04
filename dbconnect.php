ef7d4b6f98769b1b0ffc340bd195d5942f5a2c80
Akifffff <abdakif12@gmail.com>
2023-04-04 21:51:59 +0800

    connect database

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+
+	define('SERVERNAME','localhost');
+	define('USERNAME','root');
+	define('PASSWORD','');
+	define('DBNAME','db_bungasireh');
+
+	$conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+
+	
+?>
---
+++ loginprocess.php
@@ -0,0 +1,33 @@
+<?php
+//Start session
+session_start();
+
+
+include("dbconnect.php");
+
+$fuser = $_POST['fuser'];
+$fpwd = $_POST['fpwd'];
+
+$sql1 = "SELECT * FROM user WHERE username = '$fuser'"; //username exist ke tak
+$result1 = mysqli_query($conn, $sql1); // run statement $sql1
+$row1 = mysqli_fetch_array($result1);
+$count1 = mysqli_num_rows($result1);
+
+if($count1 > 0) //username wujud
+{
+    if($fpwd == $row1['password']) //kalau password betul
+    {
+        $_SESSION['fuser'] = session_id();
+        $_SESSION['fuser'] = $fuser;
+        header('Location: homepage.php'); //page lepas login
+
+    }
+    else
+    {
+        header("Location: loginpage.php?invalid=true");
+    }
+}
+else //username tak wujud
+{
+    header("Location: loginpage.php?takde=true");
+}
